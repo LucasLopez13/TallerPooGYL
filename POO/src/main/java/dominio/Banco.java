0ebe80cb36fc1798b354f94fc0e8a73f88379c0d
LucasLopez13 <lopezlucasdev@gmail.com>
2026-04-13 18:25:42 -0300

    Feature/1304/Se agrego usuarios bases para las sucursales

--- POO/src/main/java/dominio/Banco.java
+++ POO/src/main/java/dominio/Banco.java
@@ -8,9 +8,15 @@ public class Banco {
     private List<Sucursal> sucursales = new ArrayList<>();
 
     public Banco() {
-        sucursales.add(new Sucursal("Sucursal Boedo"));
-        sucursales.add(new Sucursal("Sucursal Caballito"));
-        sucursales.add(new Sucursal("Sucursal Once"));
+        Sucursal boedo = new Sucursal("Sucursal Boedo");
+        Sucursal caballito = new Sucursal("Sucursal Caballito");
+        Sucursal once = new Sucursal("Sucursal Once");
+
+        sucursales.add(boedo);
+        sucursales.add(caballito);
+        sucursales.add(caballito);
+
+        cargarDatosEnSucursales(boedo,caballito,once);
     }
 
     //Buscar una cuenta por su email
@@ -35,5 +41,67 @@ public class Banco {
     //Getter de las cuentas
     public List<Sucursal> getSucursales() {
         return sucursales;
+    }
+
+    public void cargarDatosEnSucursales(Sucursal boedo, Sucursal caballito, Sucursal once) {
+        boedo.registrarCuenta(new CuentaBuilder()
+                .conNombre("Lucas López")
+                .conEdad(22)
+                .conEmail("lucas@gmail.com")
+                .conPassword("1234")
+                .conDireccion("San Juan 123")
+                .conTipo(TipoDeCuenta.AHORRO)
+                .conSucursal(boedo)
+                .construir());
+
+        boedo.registrarCuenta(new CuentaBuilder()
+                .conNombre("Sofia Martinez")
+                .conEdad(25)
+                .conEmail("sofia@gmail.com")
+                .conPassword("1234")
+                .conDireccion("Avenida Boedo 456")
+                .conTipo(TipoDeCuenta.CORRIENTE)
+                .conSucursal(boedo)
+                .construir());
+
+        caballito.registrarCuenta(new CuentaBuilder()
+                .conNombre("Martin Gomez")
+                .conEdad(30)
+                .conEmail("martin@gmail.com")
+                .conPassword("1234")
+                .conDireccion("Rivadavia 4500")
+                .conTipo(TipoDeCuenta.SUELDO)
+                .conSucursal(caballito)
+                .construir());
+
+        caballito.registrarCuenta(new CuentaBuilder()
+                .conNombre("Valeria Sanchez")
+                .conEdad(28)
+                .conEmail("valeria@gmail.com")
+                .conPassword("1234")
+                .conDireccion("Acoyte 120")
+                .conTipo(TipoDeCuenta.AHORRO)
+                .conSucursal(caballito)
+                .construir());
+
+        once.registrarCuenta(new CuentaBuilder()
+                .conNombre("Diego Torres")
+                .conEdad(45)
+                .conEmail("diego@gmail.com")
+                .conPassword("1234")
+                .conDireccion("Corrientes 2500")
+                .conTipo(TipoDeCuenta.CORRIENTE)
+                .conSucursal(once)
+                .construir());
+
+        once.registrarCuenta(new CuentaBuilder()
+                .conNombre("Camila Fernandez")
+                .conEdad(33)
+                .conEmail("camila@gmail.com")
+                .conPassword("1234")
+                .conDireccion("Pueyrredon 300")
+                .conTipo(TipoDeCuenta.SUELDO)
+                .conSucursal(once)
+                .construir());
     }
 }
